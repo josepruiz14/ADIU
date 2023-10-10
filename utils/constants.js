7e447ade07f5e57bd8e1fab5d37e1a24f0071e11
josepruiz14 <78271978+josepruiz14@users.noreply.github.com>
2023-10-10 16:10:31 +0200

    Add files via upload

---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+export const constants = {
+  MOTOGP_BASE_URL: "https://racingmike.com/api/v1.0/",
+  ENPOINTS: {
+    SEASONS: "motogp-season",
+    STANDING_RIDERS: "motogp-world-standing-riders",
+  },
+  TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+  MOTOGP_CATEGORY_ID: "e8c110ad-64aa-4e8e-8a86-f2f152f6a942",
+};
